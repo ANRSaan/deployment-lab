5cc652ad667a2fc17e63a48c69702bc5b4ec3b69
Brian Williams <heffergod@gmail.com>
2022-09-21 13:24:33 -0700

    build ready to deploy to Herokuto go live

---
+++ client/main.js
@@ -0,0 +1,2 @@
+console.log('Hello, mofo')
+axios.post('/register')
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/', express.static(path.join(__dirname, '../client/index.html')))
+app.use(express.static(path.join(__dirname, '../client')))
+
+app.listen(port, ()=> {
+    console.log('Docked at port 4000')
+})
